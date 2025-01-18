5d998a136ec2cba07f4dcb6a41e4911e57e97351
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-18 10:29:21 -0300

    ex009 - Condição simples

---
+++ Exercicios/ex009/ex009.js
@@ -0,0 +1,7 @@
+var vel = 78
+const lim = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+if (vel > lim) {
+    console.log(`Você ultrapassou o limite de velocidade de ${lim}Km/h. Você foi MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança.`)
