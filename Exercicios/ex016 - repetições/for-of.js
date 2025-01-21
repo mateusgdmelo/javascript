ecc422f5bddb1bc0d191463a1f67c443868a97b3
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-21 07:30:55 -0300

    for... of

---
+++ Exercicios/ex016 - repetições/for-of.js
@@ -0,0 +1,11 @@
+// O for...of é usado para iterar sobre objetos iteráveis, como arrays, strings, sets, e maps.
+/*
+for (variável of iterável) {
+    // código a ser executado
+}
+*/
+let frutas = ["maçã", "banana", "laranja"]
+
+for (let fruta of frutas) {
+    console.log(fruta)
+}
