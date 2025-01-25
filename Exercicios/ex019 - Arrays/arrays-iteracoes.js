0cedccaf000215efd146e29ed22f85b27cc035d7
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-24 21:06:45 -0300

    ex019 - Iterações com arrays

--- Exercicios/ex019 - Arrays/arrays-iteracoes.js
+++ Exercicios/ex019 - Arrays/arrays-iteracoes.js
@@ -4,7 +4,7 @@ for (let pos=0; pos<n.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${n[pos]}`)
 }
 
-n.sort() // sort() ordena os valores como strings, resultando em uma ordem lexicográfica inadequada para números.
+n.sort((a, b) => a - b) // sort() ordena os valores como strings, resultando em uma ordem lexicográfica inadequada para números. Com função de comparação: sort((a, b) => a - b) realiza uma comparação numérica e garante a ordenação correta.
 console.log("...")
 // Percurso simplificado (for... in)
 for (let pos in n) {
