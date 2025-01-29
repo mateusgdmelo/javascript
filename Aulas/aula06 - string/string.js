9103ce59e611d273fd117aa7cd1c8e179e54e71a
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-29 20:50:22 -0300

    aula06 - strings

---
+++ Aulas/aula06 - string/string.js
@@ -0,0 +1,15 @@
+var nome = "Mateus";
+var sobrenome = "Melo";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+console.log(sobrenome);
+console.log(typeof sobrenome);
+
+console.log(nomeCompleto)
+
+// Escrevendo no documento html
+document.write('Ele disse: "Olá"')
+document.write("Ele disse: 'Olá'")
