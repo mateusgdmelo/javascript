d987f36993809c985a704330875db158db265390
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-29 20:37:46 -0300

    aula05 - number

---
+++ Aulas/aula05 - Number/number.js
@@ -0,0 +1,12 @@
+var numero = 5;
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.23;
+console.log(float);
+console.log(typeof float);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
