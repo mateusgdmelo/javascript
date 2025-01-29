3ffb3d297e8897c42f291d2917a739f757923654
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-29 20:26:48 -0300

    aula04 - variáveis

---
+++ Aulas/aula04 - variaveis/variavel.js
@@ -0,0 +1,39 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = "Pedro";
+
+console.log(teste);
+
+//Também é possível declarar uma variável sem o "var", mas prefira SEMPRE declarar variáveis com o "var". 
+nome = "João";
+
+console.log(nome);
+
+//É possível usar & e _ para nomes de variáveis
+
+var $num = 10;
+var _num = 20;
+
+console.log($num);
+console.log(_num);
+
+//O nome de uma variável não pode iniciar com números 
+
+var dia5 = "Dia 5"; 
+
+// Métodos:
+var meuPrimeiroNome = "Mateus"; // Método camelCase
+
+// Declarando com let e const
+
+let = testando = "teste";
+const ola = "Olá";
+
+// Declarand variável sem valor 
+
+var meuNome;
+console.log(meuNome);
+meuNome = "Lucas";
+console.log(meuNome);
