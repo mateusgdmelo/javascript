2d271a28c29de4e12f1deed9fa090b63437350b9
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-30 06:43:29 -0300

    aula10 - arrays

---
+++ Aulas/aula10 - arrays/arrays.js
@@ -0,0 +1,14 @@
+var arr = [5, "Mateus", true, {idade: 28, salario: 20000, profissao: "Programador"}, true, false,];
+
+console.log(arr)
+
+var arr2 = [2,3,4,5,6,7,8,]
+console.log(arr2)
+
+console.log(arr[1])
+console.log(arr2[0])
+
+arr[6] = 10
+arr2[7] = 10
+
+console.log(typeof arr)
