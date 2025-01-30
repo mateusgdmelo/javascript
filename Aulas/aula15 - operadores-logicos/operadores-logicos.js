973034e80b37124d6f53aa13625674832fedbdb7
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-30 19:42:16 -0300

    aula15 - operador lógico && ou and

---
+++ Aulas/aula15 - operadores-logicos/operadores-logicos.js
@@ -0,0 +1,16 @@
+// Operador lógico && é conhecido também como and
+var idade = 16;
+var nome = "João";
+
+if (nome === "João" && idade === 16) {
+    console.log("O João pode entrar na aula de esgrima.");
+} else {
+    console.log("Este não é o João");
+};
+
+if ((1 == 1 && 3 > 3) && true) {
+    console.log("Passou");
+} else if (nome === "João" && idade >= 14) {
+    console.log("Aqui passa!");
+};
+
