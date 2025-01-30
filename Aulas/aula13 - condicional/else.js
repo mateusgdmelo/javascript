7562e8bfb5617a702112824942b4aa72f773d89d
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-30 07:23:27 -0300

    aula13 - condicional Else e Else if

---
+++ Aulas/aula13 - condicional/else.js
@@ -0,0 +1,25 @@
+var nome = "Mateus";
+
+if (nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if (nome == "Mateus") {
+    console.log("O nome é Mateus");
+} else if (nome == "Xavier") {
+    console.log("O nome é Xavier");
+} else {
+    console.log("Ele possui outro nome.");
+};
+
+var idade = 18;
+
+if (idade > 20) {
+    console.log("Ele pode entrar na festa.");
+} else if (idade >= 18) {
+    console.log("Ele só pode entrar com autorização.");
+};
+
+if (nome == "Mateus") {
+    console.log("Teste")
+} else {
+    console.log("testando");
+};
