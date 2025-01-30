1a96d3e3e0713c8b687bcf7f4174493892476dd9
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-30 19:54:49 -0300

    aula16 - operador-logico || ou OR

---
+++ Aulas/aula16 - operadores-logicos/or.js
@@ -0,0 +1,15 @@
+// Operador lógico || ou OR
+var idade = 13;
+var nome = "João";
+
+if (nome == "João" || idade > 14) {
+    console.log("Pode entrar na aula de esgrima");
+} else {
+    console.log("Não pode entrar");
+};
+
+if (nome == "Pedro" && (30 > 20 || 10 == 10)){
+    console.log("Testando");
+} else {
+    console.log("Não entrou");
+};
