8f7060429c9359cb2ed18520d880ed0b8201daa7
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-30 20:37:11 -0300

    aula20 - estrutura for

---
+++ Aulas/aula20 - estrutra for/estrutura-for.js
@@ -0,0 +1,15 @@
+//estrutura for 
+
+for (var i = 0; i < 10; i++) {
+    console.log("Repetindo o for " + i);
+}
+
+var num = [1,2,3,4,5,6,7];
+
+for (var j = 0; j < num.length; j++) {
+    console.log(num[j]);
+}
+
+for (var x = 2; x < 1500; x *= 2) {
+    console.log(x)
+}
