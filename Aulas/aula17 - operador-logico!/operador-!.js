a59a9f7832d06652e2b5665388406285ae5756fe
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-30 20:00:48 -0300

    aula17 - operador ! ou NOT

---
+++ Aulas/aula17 - operador-logico!/operador-!.js
@@ -0,0 +1,14 @@
+// Operador lógico ! ou NOT
+var falso = false;
+var verdadeiro = true;
+
+
+if (!falso) {
+    console.log("Passou");
+}
+
+if (!verdadeiro) {
+    console.log("Passou");
+} else {
+    console.log("Aqui passa")
+}
