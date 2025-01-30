6ded9e736ed76a0f218ee0edc9e07937b794c2ef
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-30 07:07:44 -0300

    aula012 - Operadores de comparação

---
+++ Aulas/aula12 - operadores de comparacao/comparacao.js
@@ -0,0 +1,32 @@
+var idade = 18;
+var possuiCarro = 1; // 1 é igual a true em JS
+ 
+if (idade >= 18) {
+    console.log("O usuário pode fazer a carteira de habilitação.");
+};
+
+if (idade <= 17) {
+    console.log("O usuário não pode fazer a carteira de habilitação.");
+};
+
+if (possuiCarro) {
+    console.log("O usuário já pode andar de carro.");
+};
+
+var nome = "Mateus";
+
+if (nome == "Mateus") {
+    console.log("O seu nome é Mateus");
+};
+
+if (nome != "Mateus") {
+    console.log("O nome não é Mateus");
+};
+
+if (100 > 20) {
+    console.log("Passou");
+};
+
+if (20 < 100) {
+    console.log("Passou denovo");
+};
