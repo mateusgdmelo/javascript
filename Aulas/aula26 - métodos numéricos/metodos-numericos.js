68373950fa64726485a6d4e523b1bde65d3cba16
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-31 07:36:50 -0300

    aula26 - métodos numéricos

---
+++ Aulas/aula26 - métodos numéricos/metodos-numericos.js
@@ -0,0 +1,27 @@
+// parseFloat 10.02
+
+console.log(parseFloat("12.5478"));
+console.log(Number.parseFloat("15.8454946"));
+
+// parseInt
+
+console.log(parseInt("10"));
+console.log(parseInt(16.7894));
+
+// toFixed
+
+console.log(23.5484454849.toFixed(1));
+console.log(23.5484454849.toFixed(3));
+
+// isNaN
+
+console.log(isNaN("Teste"));
+console.log(isNaN(12));
+console.log(isNaN("14"));
+
+// MAX-VALUE E MIN-VALUE    
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(2.7976931348623157e+308);
