408450c5e92da0c02fbb7fdd396bde5903e712f3
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-01-31 06:59:24 -0300

    aula23 - criando uma lista manipulando o DOM

---
+++ Aulas/aula23 - for na prática/for-na-pratica.js
@@ -0,0 +1,18 @@
+var lista = ['Laranja', 'Maça', 'Pera', 'Uva', 'Abacaxi', 'Melão', 'Morango'];
+
+var listaUl = document.createElement('ul');
+var body = document.getElementsByTagName('body');
+
+body[0].appendChild(listaUl);
+
+var listaNoBody = document.getElementsByTagName('ul');
+console.log(listaNoBody[0]);
+
+for (var i=0; i<lista.length; i++) {
+
+    var liFor = document.createElement('li');
+    var textoLi = document.createTextNode(lista[i]);
+    liFor.appendChild(textoLi);
+    listaNoBody[0].appendChild(liFor);
+
+}
