b9e23246b980a136d5d51a9189fdd1b5745310ee
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-02-01 18:38:13 -0300

    aula37 - removendo elementos

---
+++ Aulas/aula37 - removendo elementos/remover-elementos.js
@@ -0,0 +1,16 @@
+// removendo elemento filho
+
+var conteiner = document.querySelector('#conteiner');
+console.log(conteiner);
+var p = document.querySelector('#conteiner p');
+console.log(p);
+
+conteiner.removeChild(p);
+
+// remover o elemento 
+
+var subtitle = document.querySelector('.subtitle');
+console.log(subtitle);
+
+subtitle.remove();
+
