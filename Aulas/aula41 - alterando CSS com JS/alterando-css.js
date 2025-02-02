92e47c4895806ab21b1435ce70bf483419fdda92
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-02-02 09:56:02 -0300

    aula41 - alterando CSS

---
+++ Aulas/aula41 - alterando CSS com JS/alterando-css.js
@@ -0,0 +1,12 @@
+// selecionar o elemento
+var title = document.querySelector('#title');
+
+// adiciona o estilo 
+title.style.color = 'red';
+
+// background-color 
+title.style.backgroundColor = 'yellow';
+
+// adicionar vários estilos
+var subtitle = document.querySelector('.subtitle');
+subtitle.style.cssText = 'color: blue; background-color: pink; font-size: 50px;'
