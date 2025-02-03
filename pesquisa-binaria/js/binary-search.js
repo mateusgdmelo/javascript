a70a9dfacdbf712c6eb43a5e2bf0285875d3fbcf
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-02-03 06:48:49 -0300

    pesquisa binária

---
+++ pesquisa-binaria/js/binary-search.js
@@ -0,0 +1,9 @@
+function pesquisaBinaria (event) {
+    event.preventDefault();
+    var inicio = Number(document.querySelector('#inicio').value);
+    var fim = Number(document.querySelector('#fim').value);
+    var num = Number(document.querySelector('#num').value);
+    var resposta = document.querySelector('#res');
+    var intervalo = fim - inicio
+    var meio = intervalo / 2
+}
