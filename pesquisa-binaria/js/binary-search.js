833989bd1b390aa0e4240b0eb4d7558445970d5e
Mateus Gomes de Melo <mateusgomessdemelo@gmail.com>
2025-02-07 08:10:54 -0300

    pesquisa-binaria

--- pesquisa-binaria/js/binary-search.js
+++ pesquisa-binaria/js/binary-search.js
@@ -1,9 +1,33 @@
 function pesquisaBinaria (event) {
-    event.preventDefault();
+   event.preventDefault(); 
+
     var inicio = Number(document.querySelector('#inicio').value);
     var fim = Number(document.querySelector('#fim').value);
     var num = Number(document.querySelector('#num').value);
     var resposta = document.querySelector('#res');
-    var intervalo = fim - inicio
-    var meio = intervalo / 2
+    var meio = Math.floor((fim + inicio) / 2) // A fórmula para busca binária
+    var cont = 1;
+
+    if (num === meio) {
+        resposta.innerHTML = `1º Tentativa. </br>`;
+        resposta.innerHTML += 'O número selecioando precisou apenas de uma tentativa para ser encontrado.';
+    } else {
+        resposta.innerHTML = `1º Tentativa. </br>`;
+        while (num !== meio) {
+
+            if (num < meio) {
+                fim = meio;
+            } else if (num > meio){
+                inicio = meio;
+
+            }
+
+            meio = Math.floor((fim + inicio) / 2);
+            
+            cont++;
+            resposta.innerHTML += `${cont}º Tentativa. </br>`;
+        };
+
+        resposta.innerHTML += `O número selecionado precisou de ${cont} tentativas para ser encontrado.`;
+    };
 }
