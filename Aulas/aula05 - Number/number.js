84b8ef25825a2be032c6ad1101f1430b2445ec4d
mateusgdmelo <mateusgomessdemelo@gmail.com>
2025-02-12 17:49:06 -0300

    Aula05 - Number

--- Aulas/aula05 - Number/number.js
+++ Aulas/aula05 - Number/number.js
@@ -6,7 +6,17 @@ var float = 5.23;
 console.log(float);
 console.log(typeof float);
 
-console.log(NaN);
+//NaN é uma palavra reservada do JavaScript que indica que um número não é um número real
+console.log(NaN); 
 console.log(typeof NaN);
+console.log(100*"olá") //Tentar fazer aritmética com uma string não numérica resultará em NaN(Não é um número)
+
 console.log(typeof +Infinity);
 console.log(typeof -Infinity);
+
+// Números extragrandes ou extrapequenos podem ser escritos com notação científica (expoente)
+let teste = document.querySelector('#teste');
+let a = 3145634e5
+let b = 13684365e-10
+
+teste.innerHTML = `${a} <br> ${b} <br>` 
