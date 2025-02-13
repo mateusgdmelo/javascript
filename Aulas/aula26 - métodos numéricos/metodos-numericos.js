f97be84365a2b557c499b356a0fb34c6a88cf233
mateusgdmelo <mateusgomessdemelo@gmail.com>
2025-02-13 07:11:41 -0300

    Aula26 - métodos numéricos

--- Aulas/aula26 - métodos numéricos/metodos-numericos.js
+++ Aulas/aula26 - métodos numéricos/metodos-numericos.js
@@ -19,12 +19,18 @@ console.log(isNaN("Teste"));
 console.log(isNaN(12));
 console.log(isNaN("14"));
 
-// MAX-VALUE E MIN-VALUE    
+// MAX-VALUE E MIN-VALUE E EPSILON
 
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_VALUE);
+console.log(Number.EPSILON); //é a diferença entre o menor número de ponto flutuante maior que 1 e 1
+console.log(Number.MAX_VALUE); //é uma constante que representa o maior número possível em JavaScript
+console.log(Number.MIN_VALUE); //é uma constante que representa o menor número possível em JavaScript
+console.log(Number.MAX_SAFE_INTEGER); //representa o número inteiro máximo seguro em JavaScript, MAX_SAFE_INTEGERé (2^53 - 1)
+console.log(Number.MIN_SAFE_INTEGER); //representa o inteiro mínimo seguro em JavaScript, MIN_SAFE_INTEGERé -(2^53 - 1)
+console.log(Number.POSITIVE_INFINITY); //Representa o Infinito positivo
+console.log(Number.NEGATIVE_INFINITY); //Representa o Infinito negativo
 
-console.log(2.7976931348623157e+308);
+console.log(2.7976931348623157e+308); //Infinito positivo
+console.log(-1/0); //Infinito negativo
 
 let teste = document.querySelector('#teste');
 
@@ -36,3 +42,4 @@ teste.innerHTML = x.toString() + //retorna um número como uma string
 "<br>" + x.toFixed(3) + //retorna uma string, com o número escrito com um número especificado de decimais
 "<br>" + x.toPrecision(6) + //retorna uma string, com um número escrito com um comprimento especificado
 "<br>" + x.valueOf() //retorna um número como um número
+
